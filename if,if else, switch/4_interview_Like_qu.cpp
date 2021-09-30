bd0a510e92fd327301f9883c737f5e09b14ad40f
Suman Malakar <malakars041@gmail.com>
2021-09-30 17:20:09 +0530

    completing conditional statements releted problems

---
+++ if,if else, switch/4_interview_Like_qu.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// first approch
+void two_in_one(int a, int b , int *sum, int *mul){
+  *sum = a+b;
+  *mul = a*b;
+}
+
+
+//second approch
+
+int main()
+{
+  int a, b, sum, mul;
+  cout << "Enter a, b  : \n";
+  cin >> a >> b;
+
+  two_in_one(a, b,&sum, &mul);
+
+  cout << "The sum is : " << sum<<"\n";
+  cout << "The multiplication is : " << mul;
+}
