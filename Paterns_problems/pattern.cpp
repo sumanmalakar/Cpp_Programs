8d8e9fa5436e4c4cd4d5edaaf41c552782daad3b
Suman Malakar <malakars041@gmail.com>
2021-10-04 13:35:21 +0530

    practicing some problems on functions

--- Paterns_problems/pattern.cpp
+++ Paterns_problems/pattern.cpp
@@ -108,41 +108,107 @@ void pattern6(int p)
 void pattern7(int n)
 {
   for(int i = 1; i<=n; i++ ){
-    for(int k = 1; k<=n-i; k++){
-      cout<<" ";
+    for(int k = 1; k<=n-i+2; k++){
+      cout<<"  ";
     }
     // cout<<endl;
     for(int j = 1; j<=i; j++){
       cout<<i-j+1<<" ";
+      }
       if(i>=2){
         for(int k =1; k<=i-1; k++){
-          cout<<k+1;
+          cout<<k+1<<" ";
         }
       }
-      // else {
-      //   cout<<j;
-      // }
-    }
+      
     cout << endl;
+    }
+  }
+
+  void pattern8(int n)
+  {
+    for (int i = 1; i <= n; i++)
+    {
+      for (int k = 1; k <= n - i + 1; k++)
+      {
+        cout << "  ";
+      }
+      // cout<<endl;
+      for (int j = 1; j <= i; j++)
+      {
+        cout << i - j + 1 <<" ";
+      }
+      if (i >= 2)
+      {
+        for (int k = 1; k <= i - 1; k++)
+        {
+          cout << k + 1 << " ";
+        }
+      }
+
+      cout << endl; 
+      if(i>n/2){
+
+
+
+      }
+
+    }
+  }
+
+  void pattern9(int n){
+    for(int i = 1; i<=n; i++){
+      for(int j=1; j<=2*n-1; j++){
+
+  // if (i >= i && j >= i && j <= 2 * n - i + 1)
+  // {
+  //   cout << n - i + 1 << " ";
+  // }
+  
+       if(i>=4 && j>=4 && j<=4){
+         cout<<n-3<<" ";
+       }
+      else if (i >= 3 && j >= 3 && j <= 5)
+       {
+         cout << n - 2 << " ";
+       }
+
+       else if (i >= 2 && j >= 2 && j <= 6)
+       {
+         cout << n - 1 << " ";
+       }
+     else{
+        cout<<n<<" ";
+        }
+      }
+      cout<<endl;
+    }
   }
 
 
-}
+
 
 int main()
 {
 
   int n;
-  // pattern1(n=5);
-  // cout << endl;
-  // pattern2(n=5);
-  // cout << endl;
-  // pattern3(n=5);
-  // cout << endl;
-  // pattern4(n=5);
-  // cout << endl;
-  // pattern5(n = 11);
-  pattern7(n = 5);
+  pattern1(n=5);
+  cout << endl;
+  pattern2(n=5);
+  cout << endl;
+  pattern3(n=5);
+  cout << endl;
+  pattern4(n=5);
+  cout << endl;
+  pattern5(n = 11);
+  cout << endl;
+  pattern6(n = 11);
+  cout << endl;
+  pattern7(n = 11);
+  cout << endl;
+  pattern8(n = 5);
+  cout << endl;
+  pattern9(n = 4);
 
   return 0;
 }
